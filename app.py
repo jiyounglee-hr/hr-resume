35c44ac53386217e1bfddbb45787a72c576aecaa
jiyounglee-hr <2074titi@naver.com>
2025-05-02 10:37:38 +0900

    Update app.py with latest changes

--- app.py
+++ app.py
@@ -738,7 +738,7 @@ with st.sidebar:
     
     st.markdown('</div>', unsafe_allow_html=True)
     st.markdown('<div class="label-text"><a href="https://career.neurophet.com/recruit" target="_blank" class="web-link">🔗 채용공고(뉴로핏 커리어) </a></div>', unsafe_allow_html=True)
-    st.markdown('<div class="label-text"><a href="https://neurophet.sharepoint.com/sites/HR2/Shared%20Documents/Forms/AllItems.aspx?as=json&id=%2Fsites%2FHR2%2FShared%20Documents%2F%EC%B1%84%EC%9A%A9&viewid=f1a0986e%2Dd990%2D4f37%2Db273%2Dd8a6df2f4c40" target="_blank" class="web-link">🔗후보자 이력서 링크</a></div>', unsafe_allow_html=True)
+    st.markdown('<div class="label-text"><a href="https://neurophet.sharepoint.com/sites/HR2/SitePages/%EC%B1%84%EC%9A%A9-%EC%A0%84%ED%98%95%EA%B4%80%EB%A6%AC.aspx" target="_blank" class="web-link">🔗후보자 이력서 링크</a></div>', unsafe_allow_html=True)
 
     # CSS 스타일 추가
     st.markdown("""
